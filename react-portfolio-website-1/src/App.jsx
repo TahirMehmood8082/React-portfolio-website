241cdefab6bb7e8e7f3544af3aa17ff14b5bd7f9
TahirMehmood8082 <infinitidev31aug22gmail.com>
2022-10-01 18:32:32 +0500

    basic structure

--- react-portfolio-website-1/src/App.jsx
+++ react-portfolio-website-1/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
 
 const App = () => {
   return (
-    <div>Tahir App</div>
+    <>
+      <Header/>
+      <Nav/>
+      <About/>
+    
+    </>
   )
 }
 
